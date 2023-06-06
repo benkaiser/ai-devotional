1a83b3021f4c1deb1f58b55a028372b1453bd181
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2023-06-06 22:23:28 +1000

    Change avatar, remove path

---
+++ assets/image/convert_avatars.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Convert avatar.png to favicon-16x16.png
+convert avatar.png -resize 16x16 favicon-16x16.png
+
+# Convert avatar.png to favicon-32x32.png
+convert avatar.png -resize 32x32 favicon-32x32.png
+
+# Convert avatar.png to favicon-57x57.png
+convert avatar.png -resize 57x57 favicon-57x57.png
+
+# Convert avatar.png to favicon-60x60.png
+convert avatar.png -resize 60x60 favicon-60x60.png
+
+# Convert avatar.png to favicon-70x70.png
+convert avatar.png -resize 70x70 favicon-70x70.png
+
+# Convert avatar.png to favicon-72x72.png
+convert avatar.png -resize 72x72 favicon-72x72.png
+
+# Convert avatar.png to favicon-76x76.png
+convert avatar.png -resize 76x76 favicon-76x76.png
+
+# Convert avatar.png to favicon-96x96.png
+convert avatar.png -resize 96x96 favicon-96x96.png
+
+# Convert avatar.png to favicon-114x114.png
+convert avatar.png -resize 114x114 favicon-114x114.png
+
+# Convert avatar.png to favicon-120x120.png
+convert avatar.png -resize 120x120 favicon-120x120.png
+
+# Convert avatar.png to favicon-128x128.png
+convert avatar.png -resize 128x128 favicon-128x128.png
+
+# Convert avatar.png to favicon-144x144.png
+convert avatar.png -resize 144x144 favicon-144x144.png
+
+# Convert avatar.png to favicon-150x150.png
+convert avatar.png -resize 150x150 favicon-150x150.png
+
+# Convert avatar.png to favicon-152x152.png
+convert avatar.png -resize 152x152 favicon-152x152.png
+
+# Convert avatar.png to favicon-180x180.png
+convert avatar.png -resize 180x180 favicon-180x180.png
+
+# Convert avatar.png to favicon-192x192.png
+convert avatar.png -resize 192x192 favicon-192x192.png
+
+# Convert avatar.png to favicon-310x310.png
+convert avatar.png -resize 310x310 favicon-310x310.png
+
+# Convert avatar.png to favicon-384x384.png
+convert avatar.png -resize 384x384 favicon-384x384.png
+
+# Convert avatar.png to favicon-512x512.png
+convert avatar.png -resize 512x512 favicon-512x512.png
+
+# Convert avatar.png to favicon.ico
+convert avatar.png -resize 16x16 -background transparent -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
